eac939ef723797eedf5e07df8ee3ccfd08cf883b
Ruba ALFATIH <rubaalfatih@MacBook-Air-de-Ruba.local>
2025-12-07 16:31:46 +0100

    just a premer push

---
+++ commande.py
@@ -0,0 +1,13 @@
+print("""
+rm -r "nom du dossier -> supprimer un dossier 
+rm -i -> te demandera l'autorisation 
+rm "nom d'element"
+ifconfig -> savoir ton IP 
+utlisiteur@son_IP pour se conneceter à une machine à ditance via le ssh
+pKill -9 nom_du programme pour touer un programme en utilisant son nom
+kill -9 PID pour touer un prossesseus en utilsant le PID 
+ 
+""")
+print("just a test")
+print("just a test")
+print("just a test")
